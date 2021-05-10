d62e2cbb6aac4da4912948a2a14ac0199937e776
punit <punit@suman.com>
2021-05-10 23:35:16 +0530

    bug fix

--- geometry/geometry.go
+++ geometry/geometry.go
@@ -1,5 +1,10 @@
 package geometry
 
-func RectangleArea(a, b float64) float64 {
-	return a * b
+import "errors"
+
+func RectangleArea(a, b float64) (float64, error) {
+	if a <= 0 || b <= 0 {
+		return 0, errors.New("enter only positive values")
+	}
+	return a * b, nil
 }
